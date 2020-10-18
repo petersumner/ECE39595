94b9b656c7653b26b4bb412f5117097b2e4984a3
Peter <petersumner515@gmail.com>
2020-10-18 12:37:05 -0400

    add keyevent override

--- Project/game/ObjectDisplayGrid.java
+++ Project/game/ObjectDisplayGrid.java
@@ -2,12 +2,16 @@ package game;
 
 import asciiPanel.AsciiPanel;
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.awt.event.*;
 
 public class ObjectDisplayGrid extends JFrame implements KeyListener, InputSubject {
     
     private static int height;
     private static int width;
     private static AsciiPanel terminal;
+    private List<InputObserver> inputObservers = null;
 
     public ObjectDisplayGrid(int _width,  int _height){
         width = _width;
@@ -19,6 +23,18 @@ public class ObjectDisplayGrid extends JFrame implements KeyListener, InputSubje
 
     @Override
     public void registerInputObserver(InputObserver observer){
-        InputObservers.add(observer);
+        inputObservers.add(observer);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e){
+        KeyEvent keypress = (KeyEvent) e;
+        notifyInputObservers(keypress.getKeyChar());
+    }
+
+    private void notifyInputObservers(char ch) {
+        for(InputObserver observer : inputObservers){
+            observer.observerUpdate(ch);
+        }
     }
 }
