2a01a3d502324c2e3ba6e5dc4716510d600f24d2
Peter <petersumner515@gmail.com>
2020-10-18 12:48:46 -0400

    objectdisplaygrid functions

--- Project/game/ObjectDisplayGrid.java
+++ Project/game/ObjectDisplayGrid.java
@@ -7,11 +7,14 @@ import java.util.List;
 import java.awt.event.*;
 
 public class ObjectDisplayGrid extends JFrame implements KeyListener, InputSubject {
-    
+
+    private static final long serialVersionUID = 1L;
     private static int height;
     private static int width;
     private static AsciiPanel terminal;
     private List<InputObserver> inputObservers = null;
+    private static final String CLASSID = ".ObjectDisplayGrid";
+    private Char[][] objectGrid = null;
 
     public ObjectDisplayGrid(int _width,  int _height){
         width = _width;
@@ -46,5 +49,34 @@ public class ObjectDisplayGrid extends JFrame implements KeyListener, InputSubje
 
     public final void initializeDisplay(){
         Char ch = new Char('.');
+        for(int i = 0; i < width; i++){
+            for(int j = 0; j < height; j++){
+                addObjectToDisplay(ch, i, j);
+            }
+        }
+        terminal.repaint();
+    }
+
+    public void fireUp(){
+        if(terminal.requestFocusInWindow()){
+            System.out.println(CLASSID + ".ObjectDisplayGrid(...) requestFocusInWindow Succeeded");
+        } else {
+            System.out.println(CLASSID + ".ObjectDisplayGric(...) requestFocusInWindow FAILED");
+        }
+    }
+
+    public void addObjectToDisplay(Char ch, int x, int y){
+        if((0 <= x) && (x < objectGrid.length)) {
+            if((0 <= y) && (y < objectGrid[0].length)) {
+                objectGrid[x][y] = ch;
+                writeToTerminal(x, y);
+            }
+        }
+    }
+
+    private void writeToTerminal(int x, int y){
+        char ch = objectGrid[x][y].getChar();
+        terminal.write(ch, x, y);
+        terminal.repaint();
     }
 }
