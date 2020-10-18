00bc91cb6b5ee2bc63503affee8d4e794d040890
Peter <petersumner515@gmail.com>
2020-10-18 12:27:05 -0400

    add run override

--- Project/game/Rogue.java
+++ Project/game/Rogue.java
@@ -1,10 +1,33 @@
 package game;
 
-public class Rogue {
+public class Rogue implements Runnable {
+
+    private static ObjectDisplayGrid displayGrid = null;
+    private static final int WIDTH = 80;
+    private static final int HEIGHT = 40;
+
+    @Override
+    public void run() {
+        displayGrid.fireUP();
+        for(int step = 1; step < WIDTH/2; step *= 2){
+            for(int i = 0; i < WIDTH; i += step) {
+                for(int j = 0; j < HEIGHT; j += step) {
+                    displayGrid.addObjectToDisplay(new Char('X'), i, j);
+                }
+            }
+
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace(System.err);
+            }
+            displayGrid.initializeDisplay();
+        }
+    }
 
     public static void main(String args[]){
 
-        
+
     }
     
 }
