e14ea9d6b5707e8de4e36803379686a7e8cdd3ea
Peter <petersumner515@gmail.com>
2020-10-21 20:46:59 -0400

    add parser

--- Project/game/Rogue.java
+++ Project/game/Rogue.java
@@ -1,5 +1,14 @@
 package game;
 
+import src.*;
+import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.SAXException;
+
 public class Rogue implements Runnable {
 
     public static final int FRAMESPERSECOND = 60;
@@ -33,6 +42,16 @@ public class Rogue implements Runnable {
     }
 
     public static void main(String[] args) throws Exception {
+        String fileName = "src/xmlFiles/dungeon.xml";
+        SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+        try {
+            SAXParser saxParser = saxParserFactory.newSAXParser();
+            DungeonXMLHandler handler = new DungeonXMLHandler();
+            saxParser.parse(new File(fileName), handler);
+
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace(System.out);
+        }
 
         Rogue rogue = new Rogue(WIDTH, HEIGHT);
         Thread rogueThread = new Thread(rogue);
