c6829650a1cf6fe314df9d6ccd06a5c1419c251e
Peter <petersumner515@gmail.com>
2020-10-28 12:55:46 -0400

    remove unused initializers

--- Project/src/Monster.java
+++ Project/src/Monster.java
@@ -2,12 +2,12 @@ package src;
 
 public class Monster extends Creature {
 
-    public String name = null;
-    public int room = 0;
-    public int serial = 0;
-    public int posX = 0;
-    public int posY = 0;
-    public char type = 0;
+    public String name;
+    public int room;
+    public int serial;
+    public int posX;
+    public int posY;
+    public char type;
 
     public Monster(){
         System.out.println("Mosnter:");
