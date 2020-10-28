3fa5c24d27fb14ae96f27c6cd652f78ea7a8380c
Peter <petersumner515@gmail.com>
2020-10-28 18:47:03 -0400

    key listener test

---
+++ Project/game/KeyListenerTest.java
@@ -0,0 +1,60 @@
+package game;
+
+import java.awt.event.*;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class KeyListenerTest implements KeyListener, ActionListener {
+    
+    JFrame frame;
+    JTextField tf;
+    JLabel lbl;
+    JButton btn;
+
+    public KeyListenerTest(){
+        frame = new JFrame();
+        lbl = new JLabel();
+        tf = new JTextField();
+        tf.addKeyListener(this);
+        btn = new JButton("Clear");
+        btn.addActionListener(this);
+        JPanel panel = new JPanel();
+        panel.add(tf);
+        panel.add(btn);
+
+        frame.setLayout(new BorderLayout());
+        frame.add(lbl, BorderLayout.NORTH);
+        frame.add(panel, BorderLayout.SOUTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 100);
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        lbl.setText("You typed: " + e.getKeyChar());
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        lbl.setText("You pressed: " + e.getKeyChar());
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        lbl.setText("You released: " + e.getKeyCode());
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        tf.setText("");
+    }
+
+    public static void main(String args[]){
+        new KeyListenerTest();
+    }
+}
