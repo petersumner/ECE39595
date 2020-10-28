ad1fd161d30f136fea8d3def76b3fc2af4d3992b
Peter <petersumner515@gmail.com>
2020-10-28 15:20:27 -0400

    catch not needed

--- Project/game/Rogue.java
+++ Project/game/Rogue.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
-
 import org.xml.sax.SAXException;
 
 public class Rogue implements Runnable {
@@ -15,8 +14,6 @@ public class Rogue implements Runnable {
     public static final int TIMEPERLOOP = 1000000000 / FRAMESPERSECOND;
     private static ObjectDisplayGrid displayGrid = null;
     private Thread keyStrokePrinter;
-    private static final int WIDTH = 80;
-    private static final int HEIGHT = 40;
     public static Dungeon dungeon;
     public static DungeonXMLHandler handler;
     public char ch;
@@ -28,6 +25,8 @@ public class Rogue implements Runnable {
     @Override
     public void run() {
         displayGrid.fireUp();
+
+        // Display Walls
         for(int i=0; i<dungeon.rooms.size(); i++){
             for(int j=0; j<dungeon.rooms.get(i).width+1; j++){
                 displayGrid.addObjectToDisplay(new Char('X'), dungeon.rooms.get(i).posX+j, dungeon.rooms.get(i).posY-1);
@@ -38,6 +37,8 @@ public class Rogue implements Runnable {
                 displayGrid.addObjectToDisplay(new Char('X'), dungeon.rooms.get(i).posX+dungeon.rooms.get(i).width, dungeon.rooms.get(i).posY+j-1);
             }
         }
+
+        // Display Creatures
         for(int i=0; i<dungeon.creatures.size(); i++){
             if(dungeon.creatures.get(i).getClass() == Player.class){
                 displayGrid.addObjectToDisplay(new Char('@'), dungeon.creatures.get(i).posX, dungeon.creatures.get(i).posY);
@@ -45,11 +46,6 @@ public class Rogue implements Runnable {
             else{
                 ch = dungeon.creatures.get(i).type;
                 displayGrid.addObjectToDisplay(new Char(ch), dungeon.creatures.get(i).posX, dungeon.creatures.get(i).posY);
-            }
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                e.printStackTrace(System.err);
             }
         }    
     }
@@ -66,7 +62,7 @@ public class Rogue implements Runnable {
             e.printStackTrace(System.out);
         }
         dungeon = handler.dungeon;
-        Rogue rogue = new Rogue(WIDTH, HEIGHT);
+        Rogue rogue = new Rogue(dungeon.width, dungeon.gameHeight);
         Thread rogueThread = new Thread(rogue);
         rogueThread.start();
 
