e3df78e57d1bae0384741559e3f354f482660e99
Peter <petersumner515@gmail.com>
2020-10-30 14:09:48 -0400

    add parse print statements

--- Project/src/Passage.java
+++ Project/src/Passage.java
@@ -5,6 +5,9 @@ public class Passage extends Structure {
     public String name;
     public int room1;
     public int room2;
+    public int[] xArr = new int[100];
+    public int[] yArr = new int[100];
+    public int idx = 0;
     
     public Passage(){
         System.out.println("Passage:");
@@ -19,5 +22,15 @@ public class Passage extends Structure {
         room1 = _room1;
         room2 = _room2;
         System.out.println("   room1: " + room1 + "\n   room2: " + room2);
+    }
+
+    public void setPosX(int x){
+        xArr[idx] = x;
+        System.out.println("   posX: " + x);
+    }
+
+    public void setPosY(int y){
+        yArr[idx++] = y;
+        System.out.println("   posY: " + y);
     }
 }
