55436b4b5edd721dd1dbd43e774f377f62e08277
Peter <petersumner515@gmail.com>
2020-10-30 13:41:39 -0400

    add setRooom

--- Project/src/Item.java
+++ Project/src/Item.java
@@ -3,9 +3,14 @@ package src;
 public class Item extends Displayable {
 
     public Creature owner;
+    public int room;
 
     public void setOwner(Creature _owner){
         owner = _owner;
+    }
+
+    public void setRoom(int _room){
+        room = _room;
     }
     
 }
