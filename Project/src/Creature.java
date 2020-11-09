3fd5b5f6930d69f3604cb766ed224f40b167bb56
Peter <petersumner515@gmail.com>
2020-11-08 22:02:43 -0500

    action array

--- Project/src/Creature.java
+++ Project/src/Creature.java
@@ -1,12 +1,17 @@
 package src;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Creature extends Displayable {
 
     public int hp;
     public int hpm;
     public CreatureAction da;
+    public List<CreatureAction> creatureActions = new ArrayList<CreatureAction>();
     public ItemAction ha;
     public int room;
+
 
     public Creature(){}
 
@@ -32,5 +37,9 @@ public class Creature extends Displayable {
 
     public void setRoom(int _room){
         room = _room;
+    }
+
+    public void addCreatureAction(CreatureAction _ca){
+        creatureActions.add(_ca);
     }
 }
