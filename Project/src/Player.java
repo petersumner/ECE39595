8aa1b4dcd6e7d7b8f1fa9c3b8073510211e9ee87
Peter <petersumner515@gmail.com>
2020-11-29 12:48:45 -0500

    setDisplayedType()

--- Project/src/Player.java
+++ Project/src/Player.java
@@ -7,6 +7,7 @@ public class Player extends Creature {
     public String name = "Player";
     public int serial;
     public int hpMoves;
+    public char displayedType = '@';
 
     public Player(){
         System.out.println("Player:");
@@ -20,5 +21,9 @@ public class Player extends Creature {
     public void setArmor(Armor _armor){
         armor = _armor;
         System.out.println("   armor: " + armor.name);
+    }
+
+    public void setDisplayedType(char ch) {
+        displayedType = ch;
     }
 }
